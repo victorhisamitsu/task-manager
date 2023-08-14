3454829445c696bbe5233f0a8cc280b6ff31bdc4
@hitsa <hitsa@MacBook-Air-de-smarters-32.local>
2023-08-14 14:16:59 -0300

    add run.go

--- bootstrap/server/run.go
+++ bootstrap/server/run.go
@@ -1,1 +1,33 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/Hitsa/task-manager/bootstrap/database"
+	"github.com/Hitsa/task-manager/internal/app/note"
+	"github.com/Hitsa/task-manager/internal/app/tasks"
+)
+
+func Run() {
+	// Conectar com o DB
+
+	db := database.ConexaoDb()
+	database.CreateTables(context.Background(), db)
+
+	// Fornecer o acesso ao banco de dados para a Repository
+	NoteRepository := note.NewRepositoryNote(db)
+
+	TasksRepository := tasks.NewRepositoryTasks(db)
+
+	// Forcener a repository para a service
+	noteService := note.NewNoteService(NoteRepository)
+
+	taskService := tasks.NewTasksService(TasksRepository, noteService)
+
+	// Fornecer a service para a handler
+	router := SetupHttpServer(taskService, noteService)
+	fmt.Println(router)
+	http.ListenAndServe(":8080", router)
+}
--- main.go
+++ main.go
@@ -1,35 +1,9 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-
-	"github.com/Hitsa/task-manager/bootstrap/database"
 	"github.com/Hitsa/task-manager/bootstrap/server"
-	"github.com/Hitsa/task-manager/internal/app/note"
-	"github.com/Hitsa/task-manager/internal/app/tasks"
 )
 
 func main() {
-	// Conectar com o DB
-
-	db := database.ConexaoDb()
-	database.CreateTables(context.Background(), db)
-
-	// Fornecer o acesso ao banco de dados para a Repository
-	NoteRepository := note.NewRepositoryNote(db)
-
-	TasksRepository := tasks.NewRepositoryTasks(db)
-
-	// Forcener a repository para a service
-	noteService := note.NewNoteService(NoteRepository)
-
-	taskService := tasks.NewTasksService(TasksRepository, noteService)
-
-	// Fornecer a service para a handler
-	router := server.SetupHttpServer(taskService, noteService)
-	fmt.Println(router)
-	http.ListenAndServe(":8080", router)
-
+	server.Run()
 }
